48cc6e2dbd1fd28a79efea9f8ac24d120ed3a0ab
null <hikarudjent@gmail.com>
2024-01-23 21:33:04 +0900

    ログイン周りと、prettierの設定

--- api/server.js
+++ api/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
 require("dotenv").config();
+const cors = require("cors");
 
 const PORT = 8000;
 
-
 // APIでjsonを使うことを宣言する必要がある
+app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
 
-app.listen(PORT, () => console.log(`server is running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
