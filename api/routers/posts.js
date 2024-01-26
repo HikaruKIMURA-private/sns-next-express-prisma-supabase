75eca6ed86416640737421e5e283809687fcf3e0
null <hikarudjent@gmail.com>
2024-01-27 00:05:40 +0900

    投稿関連

---
+++ api/routers/posts.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+router.post("/post", async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({ message: "投稿内容がありません。" });
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        authorId: 1,
+      },
+    });
+    return res.status(200).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "サーバーエラーです" });
+  }
+});
+
+router.get("/get_latest_post", async (req, res) => {
+  try {
+    const latestPosts = await prisma.post.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+    res.status(200).json(latestPosts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "サーバーエラーです" });
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
 require("dotenv").config();
 const cors = require("cors");
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
