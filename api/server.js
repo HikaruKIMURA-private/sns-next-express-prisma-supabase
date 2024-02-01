2d75881a734e34d2545faa6ca086c7ded8c90272
null <hikarudjent@gmail.com>
2024-02-01 22:30:35 +0900

    ユーザー取得

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
 const postsRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
 require("dotenv").config();
 const cors = require("cors");
 
@@ -13,5 +14,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
